f276eed127f95ef1566d3d911f1dd1b8684bc752
Mygod <mygodstudio@gmail.com>
2016-06-14 17:48:06 +0800

    Need more info

--- parallel-computing/pfft.cc
+++ parallel-computing/pfft.cc
@@ -1,6 +1,7 @@
 #define __CL_ENABLE_EXCEPTIONS
 
 #include <iostream>
+#include <cassert>
 
 #include "CL/cl.hpp"
 
@@ -10,7 +11,13 @@ int main(int argc, char **argv) {
   try {
     vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    for (auto platform : platforms) cout << platform() << endl;
+    assert(!platforms.empty());
+    for (auto platform : platforms) cout
+          << "Profile: " << platform.getInfo<CL_PLATFORM_PROFILE>() << endl
+          << "Version: " << platform.getInfo<CL_PLATFORM_VERSION>() << endl
+          << "Name: " << platform.getInfo<CL_PLATFORM_NAME>() << endl
+          << "Vendor: " << platform.getInfo<CL_PLATFORM_VENDOR>() << endl
+          << "Extensions: " << platform.getInfo<CL_PLATFORM_EXTENSIONS>() << endl << endl;
     return 0;
   } catch (cl::Error error) {
     cerr << error.what() << " failed with code " << error.err() << endl;

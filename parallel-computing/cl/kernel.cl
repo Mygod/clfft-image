5a7cfcffeaec746df075bbcc5341c71a8b44c285
Mygod <mygodstudio@gmail.com>
2016-06-15 07:30:43 +0800

    Hello, world with OpenCL

---
+++ parallel-computing/cl/kernel.cl
@@ -0,0 +1,16 @@
+__kernel void hello_world(__global char *str) {
+  str[0] = 'H';
+  str[1] = 'e';
+  str[2] = 'l';
+  str[3] = 'l';
+  str[4] = 'o';
+  str[5] = ',';
+  str[6] = ' ';
+  str[7] = 'W';
+  str[8] = 'o';
+  str[9] = 'r';
+  str[10] = 'l';
+  str[11] = 'd';
+  str[12] = '!';
+  str[13] = '\0';
+}
--- parallel-computing/pfft.cc
+++ parallel-computing/pfft.cc
@@ -1,36 +1,67 @@
 #define __CL_ENABLE_EXCEPTIONS
 
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
+#include "boost/core/ignore_unused.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "boost/filesystem.hpp"
 #include "CL/cl.hpp"
 
+using namespace boost;
+using namespace boost::posix_time;
 using namespace std;
+
+typedef vector<cl::Device> Devices;
+
+void pfnNotify(const char *errinfo, const void *private_info, size_t cb, void *user_data) {
+  ignore_unused(user_data);
+  auto filename = to_iso_string(second_clock::local_time()) + ".dmp";
+  ofstream(filename).write(reinterpret_cast<const char*>(private_info), cb);
+  cerr << "Error occurred in OpenCL, details dumped to " << filename << ": " << errinfo << endl;
+}
 
 int main(int argc, char **argv) {
   try {
     vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
     assert(!platforms.empty());
+    Devices devices;
     for (auto platform : platforms) {
-      vector<cl::Device> devices;
-      platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
-      cout << "Profile: " << platform.getInfo<CL_PLATFORM_PROFILE>() << endl
-          << "Version: " << platform.getInfo<CL_PLATFORM_VERSION>() << endl
-          << "Name: " << platform.getInfo<CL_PLATFORM_NAME>() << endl
-          << "Vendor: " << platform.getInfo<CL_PLATFORM_VENDOR>() << endl
-          << "Extensions: " << platform.getInfo<CL_PLATFORM_EXTENSIONS>() << endl
-          << "Devices: " << devices.size() << endl;
-      for (auto device : devices) cout
-            << device.getInfo<CL_DEVICE_NAME>() << endl
-            << "Hardware version: " << device.getInfo<CL_DEVICE_VERSION>() << endl
-            << "Software version: " << device.getInfo<CL_DRIVER_VERSION>() << endl
-            << "OpenCL C version: " << device.getInfo<CL_DEVICE_OPENCL_C_VERSION>() << endl
-            << "Parallel compute units: " << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl << endl;
+      Devices thisDevices;
+      platform.getDevices(CL_DEVICE_TYPE_ALL, &thisDevices);
+      devices.insert(devices.end(), thisDevices.begin(), thisDevices.end());
     }
+    assert(!devices.empty());
+    assert(devices.size() == 2);
+    devices.pop_back();
+    cl::Context context(devices, nullptr, pfnNotify);
+
+    filesystem::ifstream ifs(filesystem::absolute("cl/kernel.cl", filesystem::path(argv[0]).parent_path()), ios::ate);
+    auto length = static_cast<streamoff>(ifs.tellg());
+    ifs.seekg(0, ios::beg);
+    char *programData = new char[length];
+    ifs.read(programData, length);
+    cl::Program program(context, string(move(programData)));
+    try {
+      program.build("-Werror");
+    } catch (cl::Error error) {
+      cerr << "Build failed with code " << error.err() << '.' << endl
+           << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices.front()) << endl;
+      return -2;
+    }
+
+    cl::Buffer buffer(context, CL_MEM_WRITE_ONLY, 32 * sizeof(char));
+    cl::Kernel kernel(program, "hello_world");
+    kernel.setArg(0, buffer);
+    cl::CommandQueue queue(context, devices.front());
+    queue.enqueueTask(kernel);
+    char result[32];
+    queue.enqueueReadBuffer(buffer, CL_TRUE, 0, 32 * sizeof(char), result);
+    cout << "Result: " << result << endl;
     return 0;
   } catch (cl::Error error) {
     cerr << error.what() << " failed with code " << error.err() << endl;
-    return 1;
+    return -1;
   }
 }

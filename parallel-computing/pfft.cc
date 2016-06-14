9879e7622f4df01eabe3ea26420a4e1a11631f39
Mygod <mygodstudio@gmail.com>
2016-06-14 18:02:58 +0800

    Info for devices

--- parallel-computing/pfft.cc
+++ parallel-computing/pfft.cc
@@ -12,12 +12,16 @@ int main(int argc, char **argv) {
     vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
     assert(!platforms.empty());
-    for (auto platform : platforms) cout
-          << "Profile: " << platform.getInfo<CL_PLATFORM_PROFILE>() << endl
-          << "Version: " << platform.getInfo<CL_PLATFORM_VERSION>() << endl
-          << "Name: " << platform.getInfo<CL_PLATFORM_NAME>() << endl
-          << "Vendor: " << platform.getInfo<CL_PLATFORM_VENDOR>() << endl
-          << "Extensions: " << platform.getInfo<CL_PLATFORM_EXTENSIONS>() << endl << endl;
+    auto platform = platforms.front();
+    vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+    assert(!devices.empty());
+    for (auto device : devices) cout
+          << device.getInfo<CL_DEVICE_NAME>() << endl
+          << "Hardware version: " << device.getInfo<CL_DEVICE_VERSION>() << endl
+          << "Software version: " << device.getInfo<CL_DRIVER_VERSION>() << endl
+          << "OpenCL C version: " << device.getInfo<CL_DEVICE_OPENCL_C_VERSION>() << endl
+          << "Parallel compute units: " << device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl << endl;
     return 0;
   } catch (cl::Error error) {
     cerr << error.what() << " failed with code " << error.err() << endl;
